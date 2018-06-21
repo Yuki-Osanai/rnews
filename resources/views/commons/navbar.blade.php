8e5a611d83d0a35218ddee8b052bb3649007f5c4
yuki.osanai <yuki.osanai@rakuten.com>
2018-06-21 05:10:27 +0000

    navbar2

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,28 +1,33 @@
-<header>
-    <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-              
-                <a class="navbar-brand" href="/" class="btn btn-default">R-News</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#" class="btn btn-default">My Page</a></li>
-                    <li><a href="#" class="btn btn-default">Ranking</a></li>
-                    
-                     @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+<!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-left">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+                                        <a href="#">MyPage</a>
+                                        <a href="#">Ranking</a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <a href="#" class="btn btn-default">投稿</a>
                         
-                    @endauth
-                </div>
-            @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,39 +39,7 @@
                         &nbsp;
                     </ul>
 
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-left">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-                                        <a href="#">MyPage</a>
-                                        <a href="#">Ranking</a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <a href="#" class="btn btn-default">投稿</a>
-                        
+                    @include('commons.navbar')
                         
                 </div>
             </div>
