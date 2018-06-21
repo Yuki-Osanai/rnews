45a97eeaae9e8cdca9f5f2b050b37a06d2be5abc
rena.tanaka <rena.tanaka@rakuten.com>
2018-06-21 07:11:50 +0000

    welcome added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,78 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>R-News</title>
+        
+          
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+            .container {
+                 margin-left: auto;
+                 margin-right:auto;   
             }
+            
+             .cover-img {
+                	height: 100px;
+                	display: table;
+                	width: 100%;
+                	background-size: cover;
+                }
+
+                /* ワイドスクリーン用のCSS */
+                @media only screen and (min-width: 100px) {
+            	.cover-img {
+	            	height: 800px;
+            	}
+                }
+
+                /* タブレット用のCSS */
+                @media only screen and (min-width : 768px) and (max-width : 1200px) {
+            	.cover-img {
+            		height: 500px;
+                	}
+                }
+
+                /* スマホ用のCSS */
+                @media only screen and (max-width: 479px) {
+            	.cover-img {
+            		height: 300px;
+                	}
+                }
+
+                .cover-text {
+                	display: table-cell;
+                	vertical-align: middle;
+                   text-align: center;
+                     font-size:600%;
+                     font-family :Monotype Corsiva, sans-serif;
+                     
+                }
+                
+                .square_btn {
+                    position: relative;
+                    display: inline-block;
+                    padding: 0.45em 0.9em;
+                    text-decoration: none;
+                    background: #00BCD4;/*背景色*/
+                    color: #00BCD4;/*=背景色*/
+                    box-shadow: inset 0 2px 0 rgba(255,255,255,0.2);
+                    border-bottom: solid 2px #118e9e;
+                    border-radius: 4px;
+                    text-shadow: -1px -1px rgba(255, 255, 255, 0.44), 1px 1px rgba(0, 0, 0, 0.38);
+                }
+
+                .square_btn:active {/*押したとき*/
+                    border-bottom: solid 2px #00BCD4;
+                    box-shadow: none;
+                    text-shadow: -1px -1px rgba(255, 255, 255, 0.3), 1px 1px rgba(0, 0, 0, 0.3);
+                }
+         
 
             .full-height {
                 height: 100vh;
@@ -66,7 +124,17 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+            <div class="container-fluid">
+	        <div class="row">
+	    	<div class="cover-img" style="background-image:url('back.jpg');">
+		    <div class="cover-text text-center">
+		        
+				<p style="color: #fff;">Welcome To Leutain News</p>
+				<a href="#" class="square_btn">☞Go to see</a>
+			</div>
+	    	</div>
+        	</div>
+            </div>
      
 
             
