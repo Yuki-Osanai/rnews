90af908b424d1b9762a17868787bcceacc7cfccf
Eieisyu <ec2-user@ip-172-31-24-159.us-east-2.compute.internal>
2018-06-22 01:58:10 +0000

    newspost table

---
+++ app/Http/Controllers/NewspostsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Newspost;
+
+class NewspostsController extends Controller
+{
+   
+    public function index()
+    {
+         $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $newsposts = $user->newsposts()->orderBy('created_at', 'desc')->paginate(10);
+
+            $data = [
+                'user' => $user,
+                'newsposts' => $newsposts,
+            ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else {
+            return view('welcome');
+        }
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+             'titile' => 'required|max:191',
+              'url' => 'required|max:191'
+        ]);
+
+        $request->user()->newsposts()->create([
+            'content' => $request->content,
+            'title'=> $request->title,
+            'url'=> $request->url,
+            
+            
+            
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+   
+    public function edit($id)
+    {
+        //
+    }
+
+   
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+   
+    public function destroy($id)
+     {
+        $newspost = \App\Newspost::find($id);
+
+        if (\Auth::id() === $newspost->user_id) {
+            $newspost->delete();
+        }
+
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +16,6 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('users', 'UsersController');
     Route::resource('newsposts', 'NewspostsController', ['only' => ['store', 'destroy']]);
 });
