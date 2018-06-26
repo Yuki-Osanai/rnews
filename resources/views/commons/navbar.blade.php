c588f76ddfccf1af5b6ca75e23e85e9b3be05e80
EC2 Default User <ec2-user@ip-172-31-0-231.us-east-2.compute.internal>
2018-06-26 01:52:31 +0000

    make title page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -31,7 +31,7 @@
                             <li>{!! link_to_route('signup.get', 'Sign up') !!}</li>
                             <li>{!! link_to_route('login', 'Login') !!}</li>
               @endif  
-        <li>{!! link_to_route('users.show', 'Letian🐼News', ['id' => Auth::id()]) !!}</li>  <!--timeline(toppage)にリンク先要変更-->
+        <li>{!! link_to_route('ichiran', 'Letian🐼News', ['id' => Auth::id()]) !!}</li>  <!--timeline(toppage)にリンク先要変更-->
      <li>{!! link_to_route('users.show', '✎', ['id' => Auth::id()]) !!}</li>
         
     
